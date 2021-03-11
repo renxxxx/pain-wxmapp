f0b9096d4caac599ab4b2dc9644e45ffc265f250
xuxk13046567620 <762933661@qq.com>
2021-03-11 14:49:04 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,101 @@
+// app.js
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    wx.hideTabBar()
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  // 自定义显示tabbar
+onTabBar: function(key) {
+  var _curPageArr = getCurrentPages();
+  var _curPage = _curPageArr[_curPageArr.length - 1];
+  var _pagePath = _curPage.__route__;
+  if (_pagePath.indexOf('/') != 0) {
+    _pagePath = '/' + _pagePath;
+  }
+  var tabBar = this.tabBarData[key];
+  for (var i = 0; i < tabBar.list.length; i++) {
+    tabBar.list[i].active = false;
+    if (tabBar.list[i].pagePath == _pagePath) {
+      tabBar.list[i].active = true; // 根据页面地址设置当前页面状态    
+    }
+  }
+  _curPage.setData({
+    tabBar: tabBar
+  });
+},
+tabBarData: {
+  userInfo: null,
+  pop: 2,
+  num: 0,
+  doctor: {
+    "color": "#dbdbdb",
+    "selectedColor": "#1296db",
+    "backgroundColor": "white",
+    "borderStyle": "#eee",
+    "position": "bottom",
+    "list": [
+      {
+        "pagePath": "/pages/index/index",
+        "text": "首页",
+        "iconPath": "/pages/images/sy.png",
+        "selectedIconPath": "/pages/images/sy1.png",
+        "clas": "tabbar-item",
+        "active": true
+      },
+      {
+        "pagePath": "/pages/logs/logs",
+        "text": "记录",
+        "iconPath": "/pages/images/jl.png",
+        "selectedIconPath": "/pages/images/jl1.png",
+        "clas": "tabbar-item",
+        "active": false
+      },
+      {
+        "pagePath": "/pages/mine/mine",
+        "text": "我的",
+        "iconPath": "/pages/images/wd.png",
+        "selectedIconPath": "/pages/images/wd1.png",
+        "clas": "tabbar-item",
+        "active": false
+      }
+    ]
+  },
+  plan: {
+    "color": "#dbdbdb",
+    "selectedColor": "#1296db",
+    "backgroundColor": "white",
+    "borderStyle": "white",
+    "position": "bottom",
+    "list": [
+      {
+        "pagePath": "/pages/plIndex/plIndex",
+        "text": "首页",
+        "iconPath": "/pages/images/sy.png",
+        "selectedIconPath": "/pages/images/sy1.png",
+        "clas": "tabbar-item",
+        "active": true
+      },
+      {
+        "pagePath": "/pages/plMine/plMine",
+        "text": "我的",
+        "iconPath": "/pages/images/wd.png",
+        "selectedIconPath": "/pages/images/wd1.png",
+        "clas": "tabbar-item",
+        "active": false
+      }
+    ]
+  }
+},
+  globalData: {
+    userInfo: null
+  }
+})
