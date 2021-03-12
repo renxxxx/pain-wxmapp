163e72afa293fbde62f44f010d2030e31b5b37c5
xuxk13046567620 <762933661@qq.com>
2021-03-12 17:22:07 +0800

    1

--- pages/chatNow/chatNow.js
+++ pages/chatNow/chatNow.js
@@ -6,7 +6,21 @@ Page({
    * 页面的初始数据
    */
   data: {
-    disease:{diseaseName:'',diseaseId:''}
+    userId:123,
+    disease:{diseaseName:'头疼',diseaseId:''},
+    diseaseNo:'123123123',
+    diseaseMsg:[ {messageNo:1,createTime:'上午9:00',img:'../images/wzj1.jpg',txt:'',userNo:'123'},
+    {messageNo:1,createTime:'上午9:00',img:'../images/wzj.jpg',txt:'你好吗你好吗你好吗你,好吗你好吗你好吗你好吗你好吗',userNo:'1234'},
+    {messageNo:1,createTime:'上午9:00',img:'../images/wzj1.jpg',txt:'你好吗',userNo:'1234'},
+    {messageNo:1,createTime:'上午9:00',img:'',txt:'你好吗',userNo:'123'},
+    {messageNo:1,createTime:'上午9:00',img:'../images/wzj.jpg',txt:'',userNo:'123'},
+    {messageNo:1,createTime:'3分钟前',img:'../images/wzj.jpg',txt:'你好吗',userNo:'1234'},
+    {messageNo:1,createTime:'3分钟前',img:'',txt:'你好吗,你好吗,,你好吗你好吗你好吗',userNo:'123'},
+    {messageNo:1,createTime:'3分钟前',img:'../images/wzj.jpg',txt:'',userNo:'123'},
+    {messageNo:1,createTime:'3分钟前',img:'../images/wzj.jpg',txt:'你好吗',userNo:'1234'}],
+    sendMsg:'confont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具confont-国confont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具confont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前confont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具confont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具confont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具confont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具端开发的便捷工具内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具confont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具confont-国内功能很强大且图标内容很丰富的矢量图标库,提供矢量图标下载、在线存储、格式转换等功能。阿里巴巴体验团队倾力打造,设计和前端开发的便捷工具',
+    // sendMsg:''
+    
   },
 
   /**
